c25e724139b386337c0c9771b58c8d49e1ae74da
mfarhannugroho <mfarhannugroho08@gmail.com>
2023-10-31 20:58:55 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Pertemuan9 Tugas PPPB Bottom Navigation View Fragment"
+include(":app")
+ 
